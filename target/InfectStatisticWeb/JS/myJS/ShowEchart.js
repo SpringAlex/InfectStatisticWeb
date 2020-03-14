29ef6293c81219874bce9d03692fb513ca83c38c
SpringAlex <49441298+SpringAlex@users.noreply.github.com>
2020-03-14 10:22:29 +0800

    增加省份页面跳转

--- target/InfectStatisticWeb/JS/myJS/ShowEchart.js
+++ target/InfectStatisticWeb/JS/myJS/ShowEchart.js
@@ -1,27 +1,17 @@
-// 基于准备好的dom，初始化echarts实例
 var myChart = echarts.init(document.getElementById('main'));
 
-// 指定图表的配置项和数据
-var option = {
-    title: {
-        text: 'ECharts 入门示例'
-    },
-    tooltip: {},
-    legend: {
-
-        data:['销量']
-    },
+option = {
     xAxis: {
-        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     },
-    yAxis: {},
+    yAxis: {
+        type: 'value'
+    },
     series: [{
-        name: '销量',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line'
     }]
 };
-
 // 使用刚指定的配置项和数据显示图表。
 myChart.setOption(option);
-
---
+++ target/InfectStatisticWeb/newPage/province.jsp
@@ -0,0 +1,18 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>省份页面</title>
+
+    <script type="text/javascript" ></script>
+</head>
+<body>
+<div id="wrap">
+<p>this is my province page</p>
+<!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->
+<div id="main" style="width: 600px;height:400px;margin:0 auto;"></div>
+    <script type="text/javascript" src="../JS/echarts.min.js"></script>
+    <script type="text/javascript" src="../JS/myJS/ShowEchart.js"></script>
+</div>
+<script type="text/javascript" src="../JS/svgmap/js/lib/SyntaxHighlighter.js"></script>
+</body>
+</html>
