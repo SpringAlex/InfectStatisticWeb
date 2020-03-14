e38ea7f2b6410756d366e9b792325bd08063efa4
SpringAlex <49441298+SpringAlex@users.noreply.github.com>
2020-03-14 20:35:17 +0800

    修复因修改idea配置出现部分文件异常

--- src/main/webapp/WEB-INF/javaFiles/com/test/servlet/myServlet.java
+++ src/main/webapp/WEB-INF/javaFiles/com/test/servlet/myServlet.java
@@ -1,13 +1,27 @@
 package com.test.servlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 
 public class myServlet extends javax.servlet.http.HttpServlet {
+    private String message;
+    @Override
+    public void init() throws ServletException {
+        message = "Hello world, this message is from servlet!";
+    }
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse resp) throws javax.servlet.ServletException, IOException {
+        //设置响应内容类型
+        resp.setContentType("text/html");
 
+        //设置逻辑实现
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>" + message + "</h1>");
     }
 }
